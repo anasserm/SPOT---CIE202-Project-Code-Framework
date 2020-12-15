74e7c14e927e78769d3b5b90d09ef70f04fd798e
Ahmed Yousef <74509299+aeroay@users.noreply.github.com>
2020-12-15 16:31:23 +0200

    action delete course
    need slight refinment

--- SPOT/Actions/ActionDelCourse.cpp
+++ SPOT/Actions/ActionDelCourse.cpp
@@ -1,12 +1,65 @@
 #include "ActionDelCourse.h"
-
+#include <iostream>
+using namespace std;
 ActionDelCourse::ActionDelCourse(Registrar*p) :Action(p)
 {
 }
 
 bool ActionDelCourse::Execute()
 {
-	return false;
+	GUI* pGUI = pReg->getGUI();
+	pGUI->PrintMsg("Enter Course Code to Delete: ");
+	string code = pGUI->GetSrting();
+	StudyPlan* sp = pReg->getStudyPlay();
+	vector<AcademicYear*> years = sp->getPlan();
+	int idx = 0, year_del = 0, sem_del = 0;
+	bool found = false;
+	for (int year = 0; year < years.size(); year++) // loop over years
+	{
+		sem_del = 0;
+		for (int sem = FALL; sem < SEM_CNT; sem++) // loop over semesters
+		{
+			list<Course*> listCourses = years[year]->getYearCourses((SEMESTER)sem);
+			idx = 0;
+			for (list<Course*>::iterator it = listCourses.begin(); it != listCourses.end(); ++it)
+			{
+				if ((*it)->getCode() == code)
+				{
+
+					found = true;
+					break;
+				}
+				idx++;
+			}
+			
+			if (found)
+				break;
+			sem_del++;
+		}
+		if (found)
+			break;
+		year_del++;
+
+
+	}
+
+	if (found)
+	{
+
+		StudyPlan* sp = pReg->getStudyPlay();
+		vector<AcademicYear*> years = sp->getPlan();
+		list<Course*> listCourses = years[year_del]->getYearCourses((SEMESTER)sem_del);
+
+			
+		list<Course*>::iterator it2 = listCourses.begin();
+		advance(it2, idx);
+
+		/*int x;
+		cin >> x;*/
+		listCourses.erase(it2);
+
+	}
+	return true;
 }
 
 ActionDelCourse::~ActionDelCourse()
--- SPOT/Main.cpp
+++ SPOT/Main.cpp
@@ -1,4 +1,7 @@
 #include "Registrar.h"
+#include <iostream>
+#include <list>
+#include "Courses/Course.h"
 
 
 using namespace std;
@@ -10,4 +13,43 @@ int main()
 	Registrar Reg;
 	Reg.Run();
 	return 0;
+	
+	/*
+	Course* c1 = new Course("Abdo", "Nasser", 1);
+	Course* c2 = new Course("Ahmed", "Youssif", 2);
+	Course* c3 = new Course("Mostafa", "Maged", 3);
+
+	list<Course*> l;
+	l.push_back(c1);
+	l.push_back(c2);
+	l.push_back(c3);
+
+	for (list<Course*>::iterator iter = l.begin(); iter != l.end(); ++iter)
+	{
+		cout << (*iter)->getCredits() << " ";
+	}
+
+	int idx = 0;
+	for (list<Course*>::iterator iter = l.begin(); iter != l.end(); ++iter)
+	{
+		if ((*iter)->getCode() == "Ahmed")
+			break;
+		idx++;
+	}
+	list<Course*>::iterator iter = l.begin();
+	advance(iter, idx);
+	cout << (*iter)->getCode() << endl;
+	int x;
+	cin >> x;
+	/*l.erase(iter);
+	cout << endl;
+
+	for (list<Course*>::iterator iter = l.begin(); iter != l.end(); ++iter)
+	{
+		cout << (*iter)->getCredits() << " ";
+	}
+	
+	*/
+	
+	
 }
