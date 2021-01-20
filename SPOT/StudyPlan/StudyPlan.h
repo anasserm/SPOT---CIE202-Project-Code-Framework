a478e63204547d1ad191eee616cbb122f79d439c
Mostafa-Maged723 <mostafamaged420@gamil.com>
2021-01-20 10:32:57 +0200

    Update StudyPlan.h

--- SPOT/StudyPlan/StudyPlan.h
+++ SPOT/StudyPlan/StudyPlan.h
@@ -2,16 +2,14 @@
 #include <vector>
 #include "AcademicYear.h"
 #include "../GUI/Drawable.h"
-#include <utility>
-#include <list>
 
 //A full study plan for as student
-class StudyPlan :public Drawable
+class StudyPlan:public Drawable
 {
-	int TotalCredits = 0;		//total no. of credit hours for courses registred in this year
-	int TotalUnivCredits = 0, TotalMajorCredits = 0,
-		TotalTrackCredits = 0, TotalConcentrationCredits = 0,
-		TotalMinorCredits = 0;
+	int TotalCredits=0;		//total no. of credit hours for courses registred in this year
+	int TotalUnivCredits=0, TotalMajorCredits=0,
+		TotalTrackCredits=0, TotalConcentrationCredits=0,
+		TotalMinorCredits=0;
 	static int numOfCourses;
 	vector<AcademicYear*> plan;	//plan is a vector of academic years
 
@@ -19,16 +17,13 @@ class StudyPlan :public Drawable
 public:
 	StudyPlan();
 	bool AddCourse(Course*, int year, SEMESTER);
-	bool checkPre(Course*, SEMESTER, int, vector <pair<Course_Code, double>>);
-	// the 4th parameter of the function checkPre() is a vector of pairs. Each element in this vector contains the course_code and a related decimal telling info about the year and semester
-	bool checkCo(Course*, SEMESTER, int);
 	void virtual DrawMe(GUI*) const;
-	vector<AcademicYear*> getPlan();
-	int getYearCourses(int year, SEMESTER sem)const;
+	vector<AcademicYear*> getPlan();// const;
 	virtual ~StudyPlan();
 	void setNotes(string);
 	int getCredits() const;
 	string getNotes();
 	int getNumOfCourses()const;
+	int getYearCourses(int year, SEMESTER sem);// const;
 };
 
