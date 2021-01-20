da1698c9a0a02bacc2aabed026ee105022c89f94
Mostafa-Maged723 <mostafamaged420@gamil.com>
2021-01-20 10:32:07 +0200

    Update ActionAddCourse.h

--- SPOT/Actions/ActionAddCourse.h
+++ SPOT/Actions/ActionAddCourse.h
@@ -1,21 +1,12 @@
 #pragma once
 #include "Action.h"
-#include "..\Registrar.h"
-#include "../Courses/UnivCourse.h"
-#include <utility>
-#include <list>
-#include <vector>
 //Class responsible for adding course action
-class ActionAddCourse:
+class ActionAddCourse :
 	public Action
 {
 public:
 	ActionAddCourse(Registrar*);
 	bool virtual Execute();
-	int getnCourses(int year, SEMESTER sem) const;
-	int semToX(int count, SEMESTER sem) const;
-	int yearToY(int year, int count) const;
-	int searchCourse(Course_Code code);
 	virtual ~ActionAddCourse();
 };
 
